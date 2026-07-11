cf8cc35a02f419c4ae67fbe2729e9c5d8ccc9952
KreshaJain <kreshajain10@gmail.com>
2026-07-11 20:40:54 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0],self.dfs(nums[1:]),self.dfs(nums[:-1]))
+    def dfs(self,nums):
+        rob1,rob2=0,0
+
+        for num in nums:
+            newrob = max(rob1+num,rob2)
+            rob1 = rob2
+            rob2 = newrob
+        return rob2
