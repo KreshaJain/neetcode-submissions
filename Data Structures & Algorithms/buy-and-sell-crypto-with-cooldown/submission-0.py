94826020c372e39058bb44ea3cc79fbb4a8d8684
KreshaJain <kreshajain10@gmail.com>
2026-07-22 00:14:37 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+        def dfs(i,buy):
+            if i>=len(prices):
+                return 0
+            if (i,buy) in dp:
+                return dp[(i,buy)]
+            cool = dfs(i+1,buy)
+            if buy:
+                b = dfs(i+1,not buy) - prices[i]
+                dp[(i,buy)] = max(b,cool)
+            else:
+                sell = dfs(i+2,not buy) + prices[i]
+                dp[(i,buy)] = max(sell,cool)
+            return dp[(i,buy)]
+        return dfs(0,True)
